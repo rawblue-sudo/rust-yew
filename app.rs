7a5eb0f68972cc2ec49c9bedc0caff185bf278fe
rawblue-sudo <56031736+rawblue-sudo@users.noreply.github.com>
2021-02-18 11:27:24 +0100

    added app.rs

---
+++ app.rs
@@ -0,0 +1,35 @@
+use log::info;
+use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
+
+use crate::app::modules::Header;
+use crate::app::root::Model as Root;
+
+pub struct Model {}
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        info!("App component created");
+        Model {}
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+}
+
+impl Renderable<Model> for Model {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <>
+                <div class="app">
+                <div class="title">{ "Project" }</div>
+                    <Header />
+                    <Root />
+                </div>
+            </>
+        }
+    }
+}
